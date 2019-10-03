d3cdc01eb1d0453e1b8ca42cd4e7318ad3300a5b
Emma Stotz <stotz.emma@gmail.com>
2019-10-03 08:59:08 -0400

    Added firebase database to project.

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// GLOBAL VARIABLES =========================================================
+var config = {
+  apiKey: "AIzaSyCZXCPpVoMSMu4qcyuZpxuzESi0lOlOTCo",
+    authDomain: "hogwarts-express-train.firebaseapp.com",
+    databaseURL: "https://hogwarts-express-train.firebaseio.com",
+    projectId: "hogwarts-express-train",
+    storageBucket: "",
+    messagingSenderId: "72043497661",
+    appId: "1:72043497661:web:3f042474c5f9ec1a310fd4"
+};
+
+// FUNCTIONS =================================================================
+// CODE BEGINS HERE ==========================================================
